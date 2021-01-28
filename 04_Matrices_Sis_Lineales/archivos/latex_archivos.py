b54da7e5dca0b304fb6022e58405665d3f7f645b
Jeferos <43850195+Jeferos@users.noreply.github.com>
2021-01-28 10:30:47 -0500

    Vectores: Ortogonalización de Gram-Schmidt
    Vectores: Ortogonalización de Gram-Schmidt

--- 04_Matrices_Sis_Lineales/archivos/latex_archivos.py
+++ 04_Matrices_Sis_Lineales/archivos/latex_archivos.py
@@ -2,6 +2,11 @@ from IPython.display import display, Math
 import numpy as np
 
 def ltx_matriz(m, nombre):
+    if len(np.shape(m)) == 1:
+        b = []        
+        b.append(list(m))
+        m = np.array(b)
+        
     f, g = np.shape(m)    
     dato = ''
     for i in range(f):         
